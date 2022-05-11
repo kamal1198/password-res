25914729841a7c9deea40cfe067f9b6a9a19a800
kamal1198 <kaur.preetkamal1198@gmail.com>
2022-05-11 17:43:11 -0400

    math commit

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-function generatepassword() {
 
-//Requirements Selections
+function generatePassword() {
 
-var uppercaseL = ["A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//Requirements selections
+
+var uppercaseL = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercaseL = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var character =["+","=","-","_","@","#","$","%","^","&","*","(",")"];
+var character= ["+","=","-","_","@","#","$","%","^","&","*","(",")"];
+
 uppercaseL [1]
 //provide options
-var cnum = prompt ("Choose lenth of your password");
-var numbs = confirm ("click ok if you want to include numbers");
-var uppercases = confirm ("click ok you want uppercases");
-var lowercases = confirm ("click ok if you want lowercases");
-var characters = confirm ("click ok you want characters");
+  var cnum = prompt ("Enter lenth of your password");
+  var numbs = confirm ("select if you want to include numbers");
+  var uppercases = confirm ("select if you want Uppercases");
+  var lowercases = confirm ("select if you want lowercases");
+  var characters = confirm ("Select if you want special characters");
 
-var pass = [];
+  var pass = [];
 var you = [];
 
-//Array here
-if (numbs) {
-  pass = pass.concat(num);
-}
 
-if (uppercases) {
+// Array here 
+
+if (uppercases){
   pass = pass.concat(uppercaseL);
+
 }
 
-if (lowercases) {
+if (lowercases){
   pass = pass.concat(lowercaseL);
+
 }
 
-if (characters) {
-  pass = pass.concat(lowercaseL);
+if (characters){
+  pass = pass.concat(character);
 }
 console.log(pass)
 
+
+for (var i = 0; i < cnum; i++) {
+      
+  you.push (pass[Math.floor(Math.random() * pass.length)]); 
+  }
+
+  return you.join("") ;
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -46,5 +56,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
