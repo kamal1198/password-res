45b60ce4b5371536639bfbb6b6b759ac501ea5dd
kamal1198 <kaur.preetkamal1198@gmail.com>
2022-05-11 17:15:41 -0400

    forth commit

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ var uppercaseL = ["A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
 var lowercaseL = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var character =["+","=","-","_","@","#","$","%","^","&","*","(",")"];
+uppercaseL [1]
+//provide options
+var cnum = prompt ("Choose lenth of your password");
+var numbs = confirm ("click ok if you want to include numbers");
+var uppercases = confirm ("click ok you want uppercases");
+var lowercases = confirm ("click ok if you want lowercases");
+var characters = confirm ("click ok you want characters");
 
 }
 // Write password to the #password input
